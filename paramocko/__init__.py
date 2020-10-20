bd96b7372620ab249ee8280116e510e1f5950f81
studioj <neefsj@gmail.com>
2020-10-20 16:56:13 +0200

    saving calls of exec command

--- paramocko/__init__.py
+++ paramocko/__init__.py
@@ -1,7 +1,21 @@
-import paramiko
+from paramiko import SSHClient as ParamikoSSHClient
 
 __version__ = "0.0.1"
 
 
-class SSHClient(paramiko.SSHClient):
-    pass
+class SSHClient(ParamikoSSHClient):
+    def __init__(self):
+        super().__init__()
+        self.exec_command_calls = []
+
+    def exec_command(
+        self,
+        command,
+        bufsize=-1,
+        timeout=None,
+        get_pty=False,
+        environment=None,
+    ):
+        self.exec_command_calls.append((command, bufsize, timeout, get_pty, environment))
+        retval = super(SSHClient, self).exec_command(command=command, bufsize=bufsize, timeout=timeout, get_pty=get_pty, environment=environment)
+        return retval
--- tests/test_paramocko_record.py
+++ tests/test_paramocko_record.py
@@ -1,10 +1,24 @@
 import unittest
+from unittest.mock import MagicMock
 
 from paramocko import SSHClient
 from paramiko import SSHClient as paramikoSSHClient
 
 
-class TestSSHClient(unittest.TestCase):
+class TestSSHClientRecord(unittest.TestCase):
     def testSSHClient_is_the_same_type_than_paramiko_ssh_client(self):
         paramocko_client = SSHClient()
         self.assertIsInstance(paramocko_client, paramikoSSHClient)
+
+    def test_exec_command_stores_its_calls_in_a_dict(self):
+        paramocko_client = SSHClient()
+        paramocko_client._transport = MagicMock()
+        paramocko_client.exec_command("ls")
+        self.assertEquals(paramocko_client.exec_command_calls, [("ls", -1, None, False, None)])
+
+    def test_exec_command_stills_returns_a_tuple_of_three(self):
+        paramocko_client = SSHClient()
+        paramocko_client._transport = MagicMock()
+        ret_val = paramocko_client.exec_command("ls")
+        self.assertIsInstance(ret_val, tuple)
+        self.assertEquals(3, len(ret_val))
